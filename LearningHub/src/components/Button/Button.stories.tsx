94465451e08b4c0d700f20d2104cee8029fd6000
Koblov Andrey <a.coblov@yandex.ru>
2026-01-10 09:23:17 +0400

    add Button component

--- LearningHub/src/components/Button/Button.stories.tsx
+++ LearningHub/src/components/Button/Button.stories.tsx
@@ -1,13 +1,86 @@
-import Button from './Button';
 import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
 
 const meta: Meta<typeof Button> = {
+    title: 'UI/Button',
     component: Button,
+    tags: ['autodocs'],
+    args: {
+        children: "Button",
+        variant: 'primary',
+        fontWeight: 'normal',
+        disabled: false,
+        loading: false,
+    },
+    argTypes: {
+        variant: {
+            control: 'select',
+            options: ['primary', 'outline', 'ghost', 'danger']
+        },
+        shape: { control: 'select',
+            options: ['rounded', 'circle']
+        },
+        fontWeight: {control: 'radio',
+            options: ['normal', 'bold']
+        },
+        disabled: { control: 'boolean' },
+        loading: { control: 'boolean' },
+        leftIcon: { control: false },
+        rightIcon: { control: false },
+        iconOnly: { control: 'boolean' },
+        fullWidth: {control: 'boolean' },
+    }
 }
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-    
+export const Primary: Story = {}
+
+export const Variants: Story = {
+    render: () => {
+        return (
+        <div style={{display: 'flex', gap: 10}}>
+            <Button variant='primary'>Primary</Button>
+            <Button variant='outline'>Outline</Button>
+            <Button variant='ghost'>Ghost</Button>
+            <Button variant='danger'>Danger</Button>
+        </div>
+        )
+    }
+}
+
+export const ButtonsInProject: Story = {
+    render: () => {
+        return (
+            <div style={{display: 'flex', gap: 10}}>
+                <Button variant='primary' fontWeight='bold'>Начать обучение</Button>
+                <Button variant='outline' fontWeight='bold'>Смотреть рынки</Button>
+                <Button variant='ghost' fontWeight='bold'>Начать</Button>
+                <Button variant='danger' fontWeight='bold'>СБРОСИТЬ ПОРТФЕЛЬ</Button>
+            </div>
+        )
+    }
+}
+
+export const Shapes: Story = {
+    render: () => {
+        return (
+            <div style={{display: 'flex', gap: 10}}>
+                <Button shape='circle'>111</Button>
+                <Button shape='rounded'>Rounded</Button>
+            </div>
+        )
+    }
+}
+
+export const FontWeight: Story = {
+    render: () => {
+        return (
+            <div style={{display: 'flex', gap: 10}}>
+                <Button fontWeight='normal'>Button</Button>
+                <Button fontWeight='bold'>Button</Button>
+            </div>
+        )
+    }
 }
--- LearningHub/src/components/Button/Button.tsx
+++ LearningHub/src/components/Button/Button.tsx
@@ -1,3 +1,58 @@
-export const Button = () => {
-    
+import { type ButtonHTMLAttributes, type ReactNode } from "react";
+import styles from './button.module.css';
+import clsx from "clsx";
+
+type ButtonVariant = 'primary' | 'outline' | 'ghost' | 'danger';
+type ButtonShape = 'rounded' | 'circle';
+type ButtonFontWeight = 'normal' | 'bold';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    variant?: ButtonVariant;
+    shape?: ButtonShape;
+    fullWidth?: boolean;
+    leftIcon?: ReactNode;
+    rightIcon?: ReactNode;
+    iconOnly?: boolean;
+    fontWeight?: ButtonFontWeight;
+    disabled?: boolean;
+    loading?: boolean;
+    children?: ReactNode;
+}
+
+export const Button = ({
+    variant = 'primary',
+    shape = 'rounded',
+    fontWeight = 'normal',
+    disabled,
+    leftIcon,
+    rightIcon,
+    iconOnly,
+    loading,
+    fullWidth,
+    children,
+    ...props
+}: ButtonProps) => {
+    const isDisabled = disabled || loading;
+
+    return (
+        <button
+            {...props}
+            disabled={isDisabled}
+            className={clsx(
+                styles.button,
+                styles[`button--${variant}`],
+                styles[`button--${shape}`],
+                styles[`button--${fontWeight}`],
+                fullWidth && styles['button--fullWidth'],
+                iconOnly && styles['button--iconOnly'],
+                loading && styles['button--loading'],
+            )}
+            aria-busy={loading || undefined}
+        >
+            {loading ? <span>Загрузка...</span> : null}
+            {!loading && leftIcon ? <span className={clsx(styles.button__icon, styles.button__left)}>{leftIcon}</span> : null}
+            {!loading && !iconOnly ? <span className={styles.button__label}>{children}</span> : null}
+            {!loading && rightIcon ? <span className={clsx(styles.button__icon, styles.button__right)}>{rightIcon}</span> : null}
+        </button>
+    )
 }
